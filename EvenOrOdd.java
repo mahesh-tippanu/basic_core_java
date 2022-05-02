93e5d3b9516be81f03c042337d8837bb128ef523
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-05-02 09:57:04 +0500

    added EvnOrOdd

---
+++ EvenOrOdd.java
@@ -0,0 +1,17 @@
+package Basic_core_java;
+
+import java.util.Scanner;
+
+public class EvenOrOdd {
+    public static void check(int n) {
+        if(n%2==0) System.out.println("The number "+n+" is even");
+        else System.out.println("The number "+n+" is odd");
+    }
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = sc.nextInt();
+        check(num);
+    }
+}
